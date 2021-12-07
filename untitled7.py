d64179bfec4beb889ac9f920bb2b94c61c50f8ef
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-07 11:16:08 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -7,7 +7,7 @@ Created on Tue Nov 23 14:58:19 2021
 
 #Import the libraries
 import pandas as pd
-from sklearn.metrics import accuracy_score
+#from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
