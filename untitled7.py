2619b485cc5727bf9b4dc59fdc9002d416573460
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-07 13:07:46 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -24,7 +24,7 @@ st.write("""
          """)
          
 #Open and display an image 
-image = Image.open('C:/Users/Cu Chi/Dropbox/My PC (CuChi)/Desktop/Web-based GUI/Figure_set/Picture1.png')
+image = Image.py('https://github.com/TeckVo/GUI-design/blob/main/Figure_set/Picture1.png')
 st.image(image, caption='Major components in a simulated microgrid',use_column_width=True)
 
 df = pd.read_csv('https://github.com/TeckVo/GUI-design/blob/main/Data_set/Data.csv')
