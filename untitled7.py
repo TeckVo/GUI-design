c878b6f7f6720aaea66915d65c0f880b4e100a5f
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-14 11:11:44 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -123,7 +123,7 @@ if  col2.button('Click me'):
     #X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=0, test_size=0.25)
     #RandomForestClassifier = RandomForestClassifier()
     #RandomForestClassifier.fit(X_train, Y_train)
-    col4.metric('Comfort level', '99.98 %', '0.02 %')
+    col4.metric('Comfort level', '99.98 %', '11.08 %')
     col4.metric('Operating cost', '110.54 $', '-44.04 %')
     if app_model == 'ESS':
          col3.caption(f"{app_model} system")
