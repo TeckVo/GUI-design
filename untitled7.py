fd00bc19c05a847ac2a39f6c5c2b2aac6bd5805a
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-14 11:59:49 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -83,8 +83,8 @@ elif app_model == 'Capacity':
 
 def get_user_input():
     #st.sidebar.header('Customized input data')
-    minTime = time(00,00)
-    maxTime = time(23,00)
+    minTime = time(07,00)
+    maxTime = time(06,00)
     defaultMin = time(00,00)
     defaultMax = time(00,00)
     Interruption_time = col2.slider('Interruption_time', min_value=minTime, max_value=maxTime,value=(defaultMin, defaultMax), format="LT")
