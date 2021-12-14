62129051533487447d6b06f4d652fbe2d0427acb
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-14 15:27:29 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -81,7 +81,7 @@ elif app_model == 'Capacity':
     col1.area_chart(capacity_data)
          
          
-uploaded_files = col1.file_uploader("Choose a CSV file", accept_multiple_files=True)
+uploaded_files = col1.file_uploader("Choose a new CSV file data", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
      bytes_data = uploaded_file.read()
      st.write("filename:", uploaded_file.name)
