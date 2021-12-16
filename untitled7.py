dbf90efe48fdb758a719cb68125317551776fdaf
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-16 14:57:20 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -141,17 +141,7 @@ user_input = get_user_input()
         
 
 
-@st.cache
-def load_data(nrows):
-     data = pd.read_csv('https://raw.githubusercontent.com/TeckVo/GUI-design/main/Data_set/Discharging%20ESS.csv', nrows=nrows)
-     return data
-weekly_data = load_data(96)
-df_1 = pd.DataFrame(weekly_data[:96],columns = ['ESS1','ESS2'], index=pd.RangeIndex(100, name='x')  
-df_1 = df_1.reset_index().melt('x', var_name='ESS', value_name='y')
-line_chart_1 = alt.Chart(df_1).mark_line().encode(
-    alt.X('x', title='Time slot [min]'),
-    alt.Y('y', title='Discharging power [MW]'),
-    color='ESS:N').properties(title='ESS scheduling')
+
                     
 @st.cache
 def load_center_data(nrows):
@@ -192,7 +182,19 @@ if  col2.button('Click me'):
          col3.caption(f"{app_model} system")
          with col3.expander("See explanation"):
                   st.caption("""*Discharging power amount [MW] of each ESS to enhance the microgrid resilience during the islanding period.*""")
-         col3.altair_chart(line_chart_1)
+         @st.cache
+         def load_data(nrows):
+                  data = pd.read_csv('https://raw.githubusercontent.com/TeckVo/GUI-design/main/Data_set/Discharging%20ESS.csv', nrows=nrows)
+                   return data
+         weekly_data = load_data(96)
+         df_1 = pd.DataFrame(weekly_data[:96],columns = ['ESS1','ESS2'], index=pd.RangeIndex(100, name='x')
+         df_1 = df_1.reset_index().melt('x', var_name='ESS', value_name='y')
+         line_chart = alt.Chart(df_1).mark_line().encode(
+                  alt.X('x', title='Time slot [min]'),
+                  alt.Y('y', title='Discharging power [MW]'),
+                  color='ESS:N').properties(title='ESS scheduling')
+         col3.altair_chart(line_chart)
+         
          #col3.line_chart(df_1)
                     
     elif app_model == 'CHP':
