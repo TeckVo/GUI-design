77d6fc36de565f56c8eea0a268af23f4820eff19
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-17 14:44:36 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -150,6 +150,7 @@ def get_user_input():
     maxTime = time(23,00)
     defaultMin = time(10,00)
     defaultMax = time(20,00)
+    col2.subheader('Interruption_time [H]')
     Interruption_time = col2.slider('Interruption_time [H]', min_value=minTime, max_value=maxTime,value=(defaultMin, defaultMax), format="LT")
     with col2.expander("See explanation"):
          st.caption("""*Start and end time of an extreme event that as the extreme event that makes the microgrid unable to buy power from the main grid.*""")
