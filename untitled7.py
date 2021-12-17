dccf1f8a0127e8c531bf287cfd64f42ff7f54d90
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-16 19:58:26 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -73,7 +73,14 @@ if app_model == 'Load demand':
     #col1.line_chart(base_load)
 
 
+import matplotlib.pyplot as plt
+import numpy as np
 
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
 
                
    
