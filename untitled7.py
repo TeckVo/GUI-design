538d0c7e2b853babeb206b185d88827f8f2ed4c6
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-20 14:14:29 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -260,7 +260,7 @@ if app_model == 'ESS':
          with col3.expander("See explanation"):
                   st.caption("""*Discharging power amount [MW] of each ESS to enhance the microgrid resilience during the islanding period.*""")
          #col3.line_chart(df_1)           
-    elif app_model == 'CHP':
+ elif app_model == 'CHP':
         col3.caption(f"{app_model} system: ")
        
         col3.altair_chart(line_chart_2)
