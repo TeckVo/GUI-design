5c497cdf19e899255247606f72706271fcc97b1f
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-20 14:43:27 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -249,21 +249,23 @@ else:
     st.write('Loading data....')
 
 app_model = col3.selectbox('Choose system',
-                           ['Please select system', 'ESS', 'CHP']) 
+                           ['Please select system', 'ESS', 'CHP'])
 if app_model == 'Please select system':
-         col3.caption(f"{app_model}" 
-if app_model == 'ESS':
-         col3.caption(f"{app_model} system")  
+         col3.caption(f"{app_model}")
+elif app_model == 'ESS':
+         col3.caption(f"{app_model} system")
          col3.altair_chart(line_chart_1)
          with col3.expander("See explanation"):
-                      st.caption("""*Discharging power amount [MW] of each ESS to enhance the microgrid resilience during the islanding period.*""")                  
+                  st.caption("""*Discharging power amount [MW] of each ESS to enhance the microgrid resilience during the islanding period.*""")  
+         
 elif app_model == 'CHP':
-         col3.caption(f"{app_model} system: ")
+         col3.caption(f"{app_model}")
          col3.altair_chart(line_chart_2)
          with col3.expander("See explanation"):
-                  st.caption("""*Discharging power amount [MW] of each CHP to enhance the microgrid resilience during the islanding period.*""")
-                         
-        
+                  st.caption("""*Discharging power amount [MW] of each CHP to enhance the microgrid resilience during the islanding period.*""")   
+                    
+                      
+
                       
                       
 
