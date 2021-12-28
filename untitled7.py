bf713b1e269940462bd511b6316f71affdcbe3de
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-28 11:22:36 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -246,10 +246,7 @@ if  col4.button('Run'):
                   #st.caption("""*Discharging power amount [MW] of each gas-combined heat and power system (CHP) to enhance the microgrid resilience during the islanding period.*""")
     app_model = col3.selectbox('Choose other system',
                            ['Select system','CHP'])
-    if app_model == 'Select system':
-          col3.write('Loading result....') 
-    elif app_model == 'CHP':
-          col3.altair_chart(line_chart_2)
+  
      
                
      
@@ -279,6 +276,10 @@ if  col4.button('Run'):
                   #st.caption("""*Discharging power amount [MW] of each CHP to enhance the microgrid resilience during the islanding period.*""")               
 else:
     col4.write('Loading result....')
+if app_model == 'Select system':
+         col3.write('Loading result....') 
+elif app_model == 'CHP':
+         col3.altair_chart(line_chart_2)
 
 #app_model = col3.selectbox('Choose system',
                            #['Select system','CHP'])
