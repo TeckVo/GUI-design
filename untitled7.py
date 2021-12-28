b42093aeb5790b2175e5afc0a25e8d909c64472a
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-28 11:39:21 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -241,13 +241,18 @@ if  col4.button('Run'):
     col3.altair_chart(line_chart_1)
     with col3.expander("Note"):
                   st.caption("""*Discharging power amount [MW] of each energy storage system (ESS) to enhance the microgrid resilience during the islanding period.*""")
-else:
-    col4.write('Loading result....')
     app_model = col3.selectbox('Choose other system', ['Select system','CHP'])
     if app_model == 'Select system':
-          col3.write('Loading result....')
+                  col3.write('Loading result....')
     elif app_model == 'CHP':
-          col3.altair_chart(line_chart_2)
+                  col3.altair_chart(line_chart_2)                
+else:
+    col4.write('Loading result....')
+    
+    #if app_model == 'Select system':
+          #col3.write('Loading result....')
+    #elif app_model == 'CHP':
+          #col3.altair_chart(line_chart_2)
          
 
 
