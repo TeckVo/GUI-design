0b70cab644db59760def62f288146f04a6a2bf99
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-28 11:04:26 -0500

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -237,17 +237,13 @@ if  col4.button('Run'):
                   st.caption("""* Row 0: Optimal result.""")
                   st.caption("""* Row 1: Baseline.""")
                   st.caption ("""*2. Baseline total operating cost is defined based on scenario-based stochastic programming method.*""")
-    
-
-     #col3.altair_chart(line_chart_1)
+       
+     col3.altair_chart(line_chart_2)
+     with col3.expander("Note"):
+                  st.caption("""*Discharging power amount [MW] of each gas-combined heat and power system (CHP) to enhance the microgrid resilience during the islanding period.*""")
+     col3.altair_chart(line_chart_1)
      with col3.expander("Note"):
                   st.caption("""*Discharging power amount [MW] of each energy storage system (ESS) to enhance the microgrid resilience during the islanding period.*""")
-         
-         
-     col3.altair_chart(line_chart_2)
-
-     with col3.expander("Note"):
-                  st.caption("""*Discharging power amount [MW] of each gas-combined heat and power system (CHP) to enhance the microgrid resilience during the islanding period.*""") 
                   
     #app_model = col3.selectbox('Choose system',
                            ['Select system', 'Energy storage system (ESS)', 'Gas-combined heat and power system (CHP)'])
